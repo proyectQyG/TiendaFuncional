1f0ac8b4aea252d0ae8a0c16e992970858ad7e3d
TobiasVenturelli <venturelliquimey@gmail.com>
2023-06-25 17:23:59 -0300

    Archivo utils

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from 'express'
+import Handlebars from 'handlebars'
+import handlebars from 'express-handlebars'
+import { allowInsecurePrototypeAccess } from '@handlebars/allow-prototype-access';
 import UserRouter from './routes/user.router.js'
 
 const app = express()
+
+app.engine('hbs', handlebars.engine({
+     extname: 'hbs',
+     defaultLayout: 'main',
+     handlebars: allowInsecurePrototypeAccess(Handlebars)
+}))
+app.set('view engine', 'hbs')
 const userRouter = new UserRouter()
 
 app.use('/users', userRouter.getRouter())
